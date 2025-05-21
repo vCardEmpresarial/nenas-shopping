3eb8f52c65765aa89869f99af0d49f595d18578c
vCardEmpresarial <141480022+vCardEmpresarial@users.noreply.github.com>
2025-05-21 00:43:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,9 @@ def logout():
     session.clear()
     return redirect(url_for("login"))
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
